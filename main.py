c40af1e487978cad62a051ab4f0c70ceda885a68
x-20 <x25x32x30@gmail.com>
2018-12-14 23:47:17 +0900

    don't depend on AtCoder Problems

--- main.py
+++ main.py
@@ -11,58 +11,39 @@ import twitter  # $ sudo pip3 install python-twitter
 
 
 def get_contests():
-    url = 'https://kenkoooo.com/atcoder/atcoder-api/info/contests'
-    print('[*] GET', url, file=sys.stderr)
-    resp = requests.get(url)
-    resp.raise_for_status()
-    return json.loads(resp.content)
-
-
-def get_merged_problems():
-    url = 'https://kenkoooo.com/atcoder/atcoder-api/info/merged-problems'
-    print('[*] GET', url, file=sys.stderr)
-    resp = requests.get(url)
-    resp.raise_for_status()
-    return json.loads(resp.content)
-
-
-def get_submission(url):
+    url = 'https://beta.atcoder.jp/contests/archive?lang=ja'
     print('[*] GET', url, file=sys.stderr)
     resp = requests.get(url)
     resp.raise_for_status()
     soup = bs4.BeautifulSoup(resp.content, 'lxml')
+    
+    finalpage = int(soup.find('ul', class_='pagination pagination-sm mt-0 mb-1').find_all('li')[-1].text)
+    contests = []
+    for i in range(1, finalpage+1):
+        print('[*] GET', f'{url}&page={i}', file=sys.stderr)
+        resp = requests.get(f'{url}&page={i}')
+        resp.raise_for_status()
+        tbody = bs4.BeautifulSoup(resp.content, 'lxml').find('tbody')
+        
+        for tr in tbody.find_all('tr'):
+            a = tr.find_all('a')[1]
+            contests.append([ a.text, a['href'] ])
+    
+    return contests
+ 
 
-    data = {}
-    data['url'] = url
-
-    id_, = soup.find_all('span', class_='h2')
-    assert id_.text.startswith('Submission #')
-    data['id'] = id_.text.partition('#')[2]
-
-    source_code = soup.find(id='submission-code')
-    data['souce_code'] = source_code.text
-
-    submission_info, test_cases_summary, test_cases_data = soup.find_all('table')
-
-    data['info'] = {}
-    for tr in submission_info.find_all('tr'):
-        th = tr.find('th')
-        td = tr.find('td')
-        key = th.text.replace(' ', '_').lower()
-        value = td.text.strip()
-        data['info'][key] = value
-
-    data['test_cases'] = []
-    for tr in test_cases_data.find('tbody').find_all('tr'):
-        td = tr.find_all('td')
-        data['test_cases'] += [ {
-            'case_name': td[0].text,
-            'status':    td[1].text,
-            'exec_time': td[2].text,
-            'memory':    td[3].text,
-        } ]
-
-    return data
+def can_read_submissions(contest_id):
+    resp = requests.get('https://beta.atcoder.jp'+contest_id)
+    resp.raise_for_status()
+    soup = bs4.BeautifulSoup(resp.content, 'lxml')
+    tabs = soup.find('ul', class_='nav nav-tabs').find_all('li')
+    for tab in tabs:
+        ul = tab.find('ul')
+        if not ul: continue
+        li = ul.find('li')
+        if not li: continue
+        if li.find('a', href=f'{contest_id}/submissions'): return 1
+    return 0
 
 
 def main():
@@ -85,69 +66,71 @@ def main():
         api = None
 
     # load cache
-    last_merged_problems = {}
+    shortest_codes = {}
     if args.load is not None and os.path.exists(args.load):
         with open(args.load) as fh:
-            last_merged_problems = json.load(fh)
+            shortest_codes = json.load(fh)
 
     # get data
     contests = get_contests()
-    merged_problems = get_merged_problems()
-    content_from_id = { row['id']: row for row in contests }
-
+    contest_count = len(contests)
+    contest_number = 0
+    for contest_title, contest_id in contests:
+        contest_number += 1
+        print('[*]', f'{contest_number}/{contest_count}: {contest_title}', file=sys.stderr)
+        if not can_read_submissions(contest_id): continue
+        url = f'https://beta.atcoder.jp{contest_id}/submissions'
+        resp = requests.get(url)
+        resp.raise_for_status()
+        soup = bs4.BeautifulSoup(resp.content, 'lxml')
+        tasks = soup.find('select', id='select-task').find_all('option')[1:]
+        for task in tasks:
+            task_id = task['value']
+            r = requests.get(f'{url}?f.Language=&f.Status=AC&f.Task={task_id}&f.User=&orderBy=source_length')
+            r.raise_for_status()
+            s = bs4.BeautifulSoup(r.content, 'lxml')
+            tbody = s.find('tbody')
+            if not tbody: continue
+            tds = tbody.find('tr').find_all('td')
+            problem_title = tds[1].find('a').text
+            new_user = tds[2].find('a')['href'].split('/')[2]
+            new_submission_id = tds[4]['data-id']
+            new_size = int(tds[5].text.split(' ')[0])
+            if task_id in shortest_codes:
+                old_size, old_submission_id, old_user = shortest_codes[task_id]
+                if new_size < old_size or (new_size == old_size and new_submission_id < old_submission_id):
+                    if new_user == old_user:
+                        text = f'{new_user} さんが自身のショートコードを更新しました！ ({old_size} Byte → {new_size} Byte)'
+                    else:
+                        text = f'{new_user} さんが {old_user} さんからショートコードを奪取しました！ ({old_size} Byte → {new_size} Byte)'
+                else: continue
+            else:
+                text = f'{new_user} さんがショートコードを打ち立てました！ ({new_size} Byte)'
+            shortest_codes[task_id] = [ new_size, new_submission_id, new_user ]
+            text = '\n'.join([ f'{contest_title}: {problem_title}', text, url+'/'+new_submission_id ])
+            print('[*]', text, file=sys.stderr)
+            
+            # post
+            if args.post:
+                if api is None:
+                    api = twitter.Api(
+                        consumer_key=args.consumer_key,
+                        consumer_secret=args.consumer_secret,
+                        access_token_key=args.access_token_key,
+                        access_token_secret=args.access_token_secret,
+                        sleep_on_rate_limit=True)
+                api.PostUpdate(text)
+            
+            # wait
+            time.sleep(3)
+    
     # write cache
     if args.store is not None:
         dirname = os.path.dirname(args.store)
         if not os.path.exists(dirname):
             os.makedirs(dirname)
         with open(args.store, 'w') as fh:
-            json.dump(merged_problems, fh)
-
-    # enumerate golfed submissions
-    last_problem_from_id = { row['id']: row for row in last_merged_problems }
-    for problem in merged_problems:
-        if problem['solver_count'] == 0:
-            continue
-        last_problem = last_problem_from_id.get(problem['id'], {})
-        last_submission_id = last_problem.get('shortest_submission_id')
-        if problem['shortest_submission_id'] == last_submission_id:
-            continue
-
-        # get submission
-        url = 'https://beta.atcoder.jp/contests/{shortest_contest_id}/submissions/{shortest_submission_id}'.format(**problem)
-        new_submission = get_submission(url)
-
-        contest_title = content_from_id[problem['shortest_contest_id']]['title']
-        problem_title = problem['title']
-        new_user = problem['shortest_user_id']
-        new_size = new_submission['info']['code_size']
-        if last_submission_id:
-            old_url = 'https://beta.atcoder.jp/contests/{shortest_contest_id}/submissions/{shortest_submission_id}'.format(**last_problem)
-            old_submission = get_submission(old_url)
-            old_user = last_problem['shortest_user_id']
-            old_size = old_submission['info']['code_size']
-            if new_user == old_user:
-                text = f'{new_user} さんが自身のショートコードを更新しました！ ({old_size} → {new_size})'
-            else:
-                text = f'{new_user} さんが {old_user} さんからショートコードを奪取しました！ ({old_size} → {new_size})'
-        else:
-            text = f'{new_user} さんがショートコードを打ち立てました！ ({new_size})'
-        text = '\n'.join([ f'{contest_title}: {problem_title}', text, url ])
-        print('[*]', text, file=sys.stderr)
-
-        # post
-        if args.post:
-            if api is None:
-                api = twitter.Api(
-                    consumer_key=args.consumer_key,
-                    consumer_secret=args.consumer_secret,
-                    access_token_key=args.access_token_key,
-                    access_token_secret=args.access_token_secret,
-                    sleep_on_rate_limit=True)
-            api.PostUpdate(text)
-
-        # wait
-        time.sleep(3)
+            json.dump(shortest_codes, fh)
 
 
 if __name__ == '__main__':
