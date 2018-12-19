2057c14af77b0b1a08b1eedb18f373a5ff1fc1f5
x-20 <x25x32x30@gmail.com>
2018-12-19 22:22:12 +0900

    sleep(0.5), speed up

--- main.py
+++ main.py
@@ -11,41 +11,38 @@ import twitter  # $ sudo pip3 install python-twitter
 from collections import namedtuple
 
 
-def get_contests():
-    url = 'https://beta.atcoder.jp/contests/archive?lang=ja'
-    print('[*] GET', url, file=sys.stderr)
+def get_html(url):
     resp = requests.get(url)
     resp.raise_for_status()
     soup = bs4.BeautifulSoup(resp.content, 'lxml')
-    time.sleep(1)
-    
-    finalpage = int(soup.find('ul', class_='pagination pagination-sm mt-0 mb-1').find_all('li')[-1].text)
+    time.sleep(0.5)
+    return soup
+
+
+def get_contests():
+    url = 'https://atcoder.jp/contests/archive?lang=ja'
+    print('[*] GET', url, file=sys.stderr)
+    finalpage = int(get_html(url).find('ul', class_='pagination pagination-sm mt-0 mb-1').find_all('li')[-1].text)
     contests = []
     Contest = namedtuple('Contest', [ 'title', 'path' ])
     for i in range(1, finalpage+1):
         print('[*] GET', f'{url}&page={i}', file=sys.stderr)
-        resp = requests.get(f'{url}&page={i}')
-        resp.raise_for_status()
-        tbody = bs4.BeautifulSoup(resp.content, 'lxml').find('tbody')
-        
+        tbody = get_html(f'{url}&page={i}').find('tbody')
         for tr in tbody.find_all('tr'):
             a = tr.find_all('a')[1]
             contests.append(Contest(title=a.text, path=a['href']))
-        time.sleep(1)
+        time.sleep(0.5)
     return contests
 
 
-def can_read_submissions(contest_beta_path):
-    resp = requests.get(f'https://beta.atcoder.jp{contest_beta_path}')
-    resp.raise_for_status()
-    soup = bs4.BeautifulSoup(resp.content, 'lxml')
-    tabs = soup.find('ul', class_='nav nav-tabs').find_all('li')
+def can_read_submissions(contest_path):
+    tabs = get_html(f'https://atcoder.jp{contest_path}').find('ul', class_='nav nav-tabs').find_all('li')
     for tab in tabs:
         ul = tab.find('ul')
         if not ul: continue
         li = ul.find('li')
         if not li: continue
-        if li.find('a', href=f'{contest_beta_path}/submissions'):
+        if li.find('a', href=f'{contest_path}/submissions'):
             return True
     return False
 
@@ -82,36 +79,39 @@ def main():
     contest_number = 0
     for contest in contests:
         contest_title = contest.title
-        contest_beta_path = contest.path
+        contest_path = contest.path
         contest_number += 1
         print('[*]', f'{contest_number}/{contest_count}: {contest_title}', file=sys.stderr)
-        if not can_read_submissions(contest_beta_path): continue
+        if not(contest_path in latest_submission_ids):
+            if not can_read_submissions(contest_path):
+                continue
         
-        url = f'https://beta.atcoder.jp{contest_beta_path}/submissions'
-        resp = requests.get(url)
-        resp.raise_for_status()
-        soup = bs4.BeautifulSoup(resp.content, 'lxml')
-        
+        url = f'https://atcoder.jp{contest_path}/submissions'
+        soup = get_html(url)
         tbody = soup.find('tbody')
         if not tbody: continue
-        tr = tbody.find('tr')
-        latest_submission_id = tr.find_all('td')[4]['data-id']
-        if contest_beta_path in latest_submission_ids:
-            if latest_submission_id == latest_submission_ids[contest_beta_path]:
+        submission_trs = tbody.find_all('tr')
+        latest_submission_id = submission_trs[0].find_all('td')[4]['data-id']
+        newer_submission_id = submission_trs[-1].find_all('td')[4]['data-id']
+        if contest_path in latest_submission_ids:
+            if latest_submission_id == latest_submission_ids[contest_path]:
                 continue
-        latest_submission_ids[contest_beta_path] = latest_submission_id
+        if ( not(contest_path in latest_submission_ids) ) or ( int(newer_submission_id) > int(latest_submission_ids[contest_path]) ):
+            submission_trs = []
+            tasks = soup.find('select', id='select-task').find_all('option')[1:]
+            for task in tasks:
+                task_id = task['value']
+                tbody = get_html(f'{url}?f.Language=&f.Status=AC&f.Task={task_id}&f.User=&orderBy=source_length').find('tbody')  # NOTE: this query shows submissions with the same length in the ascending order of time
+                if not tbody: continue
+                submission_trs.append(tbody.find('tr'))
+        latest_submission_ids[contest_path] = latest_submission_id
         
-        tasks = soup.find('select', id='select-task').find_all('option')[1:]
-        for task in tasks:
-            task_id = task['value']
-            resp = requests.get(f'{url}?f.Language=&f.Status=AC&f.Task={task_id}&f.User=&orderBy=source_length')  # NOTE: this query shows submissions with the same length in the ascending order of time
-            resp.raise_for_status()
-            soup = bs4.BeautifulSoup(resp.content, 'lxml')
-            tbody = soup.find('tbody')
-            if not tbody: continue
-            tds = tbody.find('tr').find_all('td')
+        for tr in submission_trs:
+            tds = tr.find_all('td')
+            if tds[6].find('span').text != 'AC': continue
             problem_title = tds[1].find('a').text
-            new_user = tds[2].find('a')['href'].split('/')[2]
+            task_id = tds[1].find('a')['href'].split('/')[-1]
+            new_user = tds[2].find('a')['href'].split('/')[-1]
             new_submission_id = tds[4]['data-id']
             new_size = int(tds[5].text.split(' ')[0])
             if task_id in shortest_codes:
@@ -144,8 +144,8 @@ def main():
                         sleep_on_rate_limit=True)
                 api.PostUpdate(text)
             
-            # wait
-            time.sleep(3)
+                # wait
+                time.sleep(3)
     
     # write cache
     if args.store is not None:
